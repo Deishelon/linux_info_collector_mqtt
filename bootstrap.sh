fbe9039785f544c24307f8c2575aa715bb9c803f
Nikita Pustovoi <deishelon@gmail.com>
2022-09-10 16:54:14 +1200

    Better exit flags in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,16 @@ set -eu
 base_path=$(dirname "$0")
 echo "Starting the linux collector service, base: ${base_path}"
 
-pushd "$base_path"
+pushd "$base_path" || exit
+
+set +eu
 source venv/bin/activate
+set -eu
+
 python3 linux_info_collector.py "$1"
+
+set +eu
 deactivate
+set -eu
+
 popd
